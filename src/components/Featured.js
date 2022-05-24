71d5077c07e9ae5a2e171b453d6acb67489212e9
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-05-25 01:43:18 +0530

    added sxios and api from coin gecko

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,14 +1,21 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import './Featured.css'
+import axios from 'axios'
 import BTC from '../assets/btc-img.png'
 import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
 const Featured = () => {
 
   const [data, setData] = useState(null);
-
-
-
   const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false"
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setData(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  })
+
   return (
     <div className='featured'>
         <div className="container">
