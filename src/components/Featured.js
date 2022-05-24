82d2e4bf112a8476ad67222ac78cd9f75f39cce7
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-05-24 23:58:09 +0530

    added bitcoin img

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './Featured.css'
+import BTC from '../assets/btc-img.png'
+import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
 const Featured = () => {
+
+  const [data, setData] = useState(null);
+
+
+
+  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false"
   return (
     <div className='featured'>
         <div className="container">
@@ -17,9 +25,14 @@ const Featured = () => {
             {/* right side  */}
             <div className="right">
                 <div className="top">
-                    hello
+                    <img src={BTC} alt="/" />
             
                 </div>
+                <div>
+                  <h5>Bitcoin</h5>
+                  <p>$49,000</p>
+                </div>
+                <span><FiArrowUpRight/>2.5%</span>
 
             </div>
         </div>
